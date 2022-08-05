c5c2b81ea02bf98e066ce8fc9a26235a03bc2e60
Guilherme Londero <97804927+guilondero@users.noreply.github.com>
2022-08-05 11:28:40 -0300

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,9 @@
+import dotenv
+import os
 
+
+dotenv.load_dotenv(dotenv.find_dotenv())     #your file need to be ".env" and stay in the same folder of your code.py, for more infos lead the librerary 
+
+userName = os.getenv("account")
+
+password = os.getenv("")
