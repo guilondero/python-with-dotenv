d0209e17f21fab72c3e2cef993ef62a9f7e47c9a
Guilherme Londero <97804927+guilondero@users.noreply.github.com>
2022-11-28 17:34:37 -0300

    Update code.py

--- code.py
+++ code.py
@@ -2,7 +2,7 @@ import dotenv
 import os
 
 
-dotenv.load_dotenv(dotenv.find_dotenv())     #your file need to be ".env" and stay in the same folder of your code.py, for more infos lead the library
+dotenv.load_dotenv(dotenv.find_dotenv())     #your file need to be ".env" and stay in the same folder of your code.py, for more infos read the library
 #in a real project your .env never goes to your repositori, this is a "secret" file with yours credencial
 
 
